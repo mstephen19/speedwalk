1a7b3783e76f33ff04c941ab3405638b97c48601
Matt Stephens <87805115+mstephen19@users.noreply.github.com>
2023-01-12 17:27:36 +0100

    feat(bundle size): package.json minification

--- main.go
+++ main.go
@@ -12,13 +12,17 @@ import (
 	"github.com/dlclark/regexp2"
 )
 
+const PackageJsonPath = "./package.json"
 const BuildPath = "./build"
 const DotJS = ".js"
 
+type Regex = string
+
 const (
-	Comments          string = `\/{2}.*`
-	MultilineComments        = `\/\*{2}(.|\n)*\*\/`
-	RemovableSpaces          = `(?<![a-zA-Z])\s|\s(?![a-zA-Z])`
+	Comments          Regex = `\/{2}.*`
+	MultilineComments Regex = `\/\*{2}(.|\n)*\*\/`
+	RemovableSpaces   Regex = `(?<![a-zA-Z])\s|\s(?![a-zA-Z])`
+	JsonSpaces        Regex = `(?<=[:{}\n\[\]],?|(",?))\s{1,}`
 )
 
 func FileExists(path string) bool {
@@ -26,21 +30,41 @@ func FileExists(path string) bool {
 	return !(err != nil)
 }
 
-func Minify(data []byte) []byte {
+func MinifyJS(data []byte) []byte {
 	// Remove all comments
 	a := regexp.MustCompile(fmt.Sprintf("%s|%s", Comments, MultilineComments)).ReplaceAll(data, []byte(""))
 	// Remove removable spaces
 	b, _ := regexp2.MustCompile(RemovableSpaces, regexp2.None).Replace(string(a), "", -1, -1)
-
 	return []byte(b)
+}
+
+func MinifyJson(data []byte) []byte {
+	a, _ := regexp2.MustCompile(JsonSpaces, regexp2.None).Replace(string(data), "", -1, -1)
+	return []byte(a)
 }
 
 func main() {
 	if !FileExists(BuildPath) {
 		panic(fmt.Sprintf("%s not found. Did you forget to compile?", BuildPath))
 	}
+	if !FileExists(PackageJsonPath) {
+		panic("package.json somehow not found.")
+	}
 
 	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+	// Minify package.json
+	go func() {
+		defer wg.Done()
+
+		data, _ := ioutil.ReadFile(PackageJsonPath)
+		file, _ := os.OpenFile(PackageJsonPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+		defer file.Close()
+		file.Write(MinifyJson(data))
+	}()
+
+	// Minify all JS files there.
 	err := filepath.Walk(BuildPath, func(path string, info fs.FileInfo, err error) error {
 		wg.Add(1)
 
@@ -60,7 +84,7 @@ func main() {
 				return
 			}
 
-			minified := Minify(data)
+			minified := MinifyJS(data)
 
 			file, _ := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
 			defer file.Close()
